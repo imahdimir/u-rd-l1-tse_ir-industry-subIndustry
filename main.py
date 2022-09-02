ea3b3245620775724159c6c304616409fad4018e
imahdimir <imahdimir@gmail.com>
2022-09-03 00:07:10 +0430

    v1

--- main.py
+++ main.py
@@ -5,15 +5,15 @@
 ##
 
 
-import pandas as pd
-import requests
-from ast import literal_eval
 import json
-from githubdata import GithubData
-from mirutil.df_utils import save_df_as_a_nice_xl as snxl
-from mirutil import utils as mu
 
-class RepAdd:
+import pandas as pd
+from githubdata import GithubData
+from mirutil import utils as mu
+from mirutil.df_utils import save_df_as_a_nice_xl as snxl
+
+
+class RepAdd :
   src = 'imahdimir/raw-d-tse_ir-industry-subIndustry'
   targ = 'imahdimir/d-tse_ir-industry-subIndustry'
 
@@ -32,7 +32,7 @@ def main() :
   ##
   fp = rp_src.local_path / 'data.json'
   ##
-  with open(fp, 'r') as f:
+  with open(fp , 'r') as f :
     data = json.load(f)
   ##
   dc = data['data']
@@ -48,23 +48,23 @@ def main() :
   ##
   df['z'] = df['z'].apply(lambda x : x['z'])
   ##
-  df = df[['Industry', 'SubIndustry', 'z']]
+  df = df[['Industry' , 'SubIndustry' , 'z']]
   ##
   df = df.explode('z')
   ##
   key_cn = {
-      't': 'CompanyName',
-      'sy' : 'Ticker',
-      'ic' : 'ic',
-      'c' : 'c',
-      's' : 's'
+      't'  : 'CompanyName' ,
+      'sy' : 'Ticker' ,
+      'ic' : 'ic' ,
+      'c'  : 'c' ,
+      's'  : 's'
       }
 
-  for ky, val in key_cn.items():
+  for ky , val in key_cn.items() :
     df[val] = df['z'].apply(lambda x : x[ky] if pd.notna(x) else None)
 
   ##
-  df.drop(columns = ['z'], inplace = True)
+  df.drop(columns = ['z'] , inplace = True)
   ##
 
   rp_tar = GithubData(ra.targ)
@@ -72,7 +72,7 @@ def main() :
   ##
   fp = rp_tar.data_fp
   ##
-  snxl(df, fp)
+  snxl(df , fp)
   ##
   tokp = '/Users/mahdi/Dropbox/tok.txt'
   tok = mu.get_tok_if_accessible(tokp)
@@ -81,7 +81,7 @@ def main() :
   msg = 'builded by: '
   msg += ra.cur_url
   ##
-  rp_tar.commit_and_push(msg, user = rp_tar.user_name, token = tok)
+  rp_tar.commit_and_push(msg , user = rp_tar.user_name , token = tok)
 
   ##
 
