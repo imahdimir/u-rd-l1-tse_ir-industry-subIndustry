ca8531b403ecacb5e60a0ad5d1db330d97ab730b
imahdimir <imahdimir@gmail.com>
2022-09-03 04:38:10 +0430

    v2

--- main.py
+++ main.py
@@ -6,11 +6,13 @@
 
 
 import json
+from datetime import date
 
 import pandas as pd
 from githubdata import GithubData
 from mirutil import utils as mu
-from mirutil.df_utils import save_df_as_a_nice_xl as snxl
+from mirutil.df_utils import read_data_according_to_type as read_data
+from mirutil.df_utils import save_as_prq_wo_index as sprq
 
 
 class RepAdd :
@@ -26,9 +28,9 @@ def main() :
   pass
 
   ##
+
   rp_src = GithubData(ra.src)
   rp_src.clone()
-
   ##
   fp = rp_src.local_path / 'data.json'
   ##
@@ -62,9 +64,12 @@ def main() :
 
   for ky , val in key_cn.items() :
     df[val] = df['z'].apply(lambda x : x[ky] if pd.notna(x) else None)
-
   ##
   df.drop(columns = ['z'] , inplace = True)
+  ##
+  df = df.drop_duplicates()
+  ##
+  df['Date'] = date.today().strftime('%Y-%m-%d')
   ##
 
   rp_tar = GithubData(ra.targ)
@@ -72,17 +77,23 @@ def main() :
   ##
   fp = rp_tar.data_fp
   ##
-  snxl(df , fp)
+  df_tar = read_data(fp)
+  ##
+  df1 = pd.concat([df_tar , df] , ignore_index = True)
+  ##
+  df1 = df1.sort_values('Date')
+  ##
+  df1 = df1.drop_duplicates(subset = df1.columns.difference(['Date']))
+  ##
+  sprq(df1 , fp)
   ##
   tokp = '/Users/mahdi/Dropbox/tok.txt'
   tok = mu.get_tok_if_accessible(tokp)
-
   ##
   msg = 'builded by: '
   msg += ra.cur_url
   ##
   rp_tar.commit_and_push(msg , user = rp_tar.user_name , token = tok)
-
   ##
 
 
